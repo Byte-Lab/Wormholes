3192bbfdeb1567ccc802fb72d5fc1110e6b335fb
BitcoderMC <bitcodermc@gmail.com>
2014-12-03 14:58:20 -0500

    Added classes
    Working on wormhole creation system

---
+++ src/main/java/io/github/bytelab/wormholes/wormholeCommands.java
@@ -0,0 +1,23 @@
+package main.java.io.github.bytelab.wormholes;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class wormholeCommands implements CommandExecutor{
+
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+
+        if(commandLabel.equalsIgnoreCase("wh")){
+
+            if(args.length != 0){
+
+            }else{
+                
+            }
+
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/io/github/bytelab/wormholes/wormholeCreate.java
@@ -0,0 +1,6 @@
+package main.java.io.github.bytelab.wormholes;
+
+public class wormholeCreate{
+
+
+}
