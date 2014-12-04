9c0bc2dd6ffa2230e0f93aecb501087dcf230cdf
BitcoderMC <bitcodermc@gmail.com>
2014-12-03 19:28:25 -0500

    Removed odd duplicate GPL license in main class (

--- src/main/java/io/github/bytelab/wormholes/main.java
+++ src/main/java/io/github/bytelab/wormholes/main.java
@@ -21,26 +21,6 @@
 package main.java.io.github.bytelab.wormholes;
 
 /*
- * Copyright (c) '2014'-2014
- * 'Byte-Lab' <'http://bytelab.pw'>
- *
- * This file is part of Wormholes.
- *
- * Wormholes is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
  * Permissions:
  * wormholes.create
  * wormholes.remove
@@ -59,19 +39,17 @@ public class main extends JavaPlugin{
     PluginDescriptionFile pdf = this.getDescription();
     Logger logger = Logger.getLogger("Minecraft");
 
-    public void onEnable(){
-
+    public void onEnable()
+    {
         getCommand("wh").setExecutor(new wormholeCommands());
 
         pluginConfiguration();
 
         logger.info("§b " + pdf.getName() + " version " + pdf.getVersion() + " has been enabled!");
-
     }
-    public void onDisable(){
-
+    public void onDisable()
+    {
         logger.info("§c " + pdf.getName() + " version " + pdf.getVersion() + " has been disabled!");
-
     }
     public void pluginConfiguration()
     {
@@ -83,6 +61,5 @@ public class main extends JavaPlugin{
 
         config.options().copyDefaults(true);
         saveConfig();
-
     }
 }
